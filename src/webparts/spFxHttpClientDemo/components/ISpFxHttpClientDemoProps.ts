b2e6ca4945391fa10482709a5fbfe4b779efcc13
habitcoach <the_habit_coach@outlook.com>
2024-02-07 07:20:57 +0530

    SharePoint_Module7_loadingData_CRUD

---
+++ src/webparts/spFxHttpClientDemo/components/ISpFxHttpClientDemoProps.ts
@@ -0,0 +1,16 @@
+import {
+  ButtonClickedCallback,
+  ICountryListItem
+} from '../../../models';
+
+export interface ISpFxHttpClientDemoProps {
+  spListItems: ICountryListItem[];
+  onGetListItems: ButtonClickedCallback;
+  onAddListItem: ButtonClickedCallback;
+onUpdateListItem: ButtonClickedCallback;
+onDeleteListItem: ButtonClickedCallback;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
